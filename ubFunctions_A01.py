b7c4f1bb0e4d67abe58315000c347770d78f8cf3
tgcodingplace <tg@tgcodingplace>
2024-12-19 11:50:30 +0100

    Erweitert um einen Kommentar

--- ubFunctions_A01.py
+++ ubFunctions_A01.py
@@ -2,11 +2,16 @@
 #
 ### Aufgabe 1:
 #
-### Entwerfen Sie eine Function anrede, die die Meldung „Guten Morgen“ ausgibt.
+### Entwerfe eine Function anrede, die die Meldung „Guten Morgen“ ausgibt.
 
 def anrede():
     print("Guten Morgen")
 
+    # Beachte, dass diese Function keinen Return-Wert besitzt, da sie die
+    # Information lediglich ausgibt
+
 # Aufruf im Hauptprogramm
 
+# Die Function liefert keinen Rückgabewert. Die Funktionalität, die von der
+# Aufgabenstellung verlangt wird, liefert die function anrede() selbst.  
 anrede() 
