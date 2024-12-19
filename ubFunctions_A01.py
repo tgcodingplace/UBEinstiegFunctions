fb778ecd8726aa20edfcf1c6b3776f14ba5e807d
tgcodingplace <tg@tgcodingplace>
2024-12-19 11:47:42 +0100

    Erster Entwurf der Aufgabe 1

---
+++ ubFunctions_A01.py
@@ -0,0 +1,12 @@
+### UB Umgang mit Functions in Python
+#
+### Aufgabe 1:
+#
+### Entwerfen Sie eine Function anrede, die die Meldung „Guten Morgen“ ausgibt.
+
+def anrede():
+    print("Guten Morgen")
+
+# Aufruf im Hauptprogramm
+
+anrede() 
