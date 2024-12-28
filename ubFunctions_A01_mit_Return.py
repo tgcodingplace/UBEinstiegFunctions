c2c88f44c1f4253d416347d74f24c50f30653d2c
tgcodingplace <tg@tgcodingplace>
2024-12-28 03:10:17 +0100

    erster Entwurf

---
+++ ubFunctions_A01_mit_Return.py
@@ -0,0 +1,20 @@
+### UB Umgang mit Functions in Python
+#
+### Aufgabe 1 - mit Rückgabewert:
+#
+### Entwerfe eine Function anrede, die die Meldung „Guten Morgen“ an den
+### Aufrufer zurückkopiert.
+
+def anrede():
+    return "Guten Morgen"
+
+    # Beachte, dass diese Function die Zeichenkette "Guten Morgen" an den
+    # Aufrufer zurückkopiert.
+
+# Aufruf im Hauptprogramm
+
+# Die function anrede() übernimmt keine Parameter und liefert über den Rückgabewert
+# die Information "Guten Morgen" zurück. Diese Information muss über die print()
+# Anweisung ausgegeben werden.  
+
+print(anrede()) 
